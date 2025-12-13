c78b5903dd0a4c78ea77fd5ca918cb09dae3ec1c
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-12-13 20:46:55 +0900

    feat: ゲーム登録APIを拡張し、初期ボードの状態をデータベースに保存する機能を追加

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,87 @@
 import express from "express";
+import "express-async-errors";
 import morgan from "morgan";
 import mysql from "mysql2/promise";
-import "express-async-errors";
 
-const port = process.env.PORT || 3000;
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
+
+const INITIAL_BOARD = [
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
+];
+
+const PORT = 3000;
+
 const app = express();
+
 app.use(morgan("dev"));
 app.use(express.static("static", { extensions: ["html"] }));
 
 app.get("/api/hello", async (req, res) => {
-  res.json({ message: "Hello, World!!!!!!!" });
+  res.json({
+    message: "Hello Express!!!",
+  });
 });
 
 app.get("/api/error", async (req, res) => {
-  throw new Error("This is a test error");
+  throw new Error("Error endpoint");
 });
 
 app.post("/api/games", async (req, res) => {
-  const startAt = new Date();
+  const now = new Date();
+
   const conn = await mysql.createConnection({
     host: "localhost",
+    database: "reversi",
     user: "reversi",
     password: "password",
-    database: "reversi",
   });
   try {
     await conn.beginTransaction();
-    await conn.execute("INSERT INTO games (started_at) VALUES (?)", [startAt]);
+
+    const gameInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      "insert into games (started_at) values (?)",
+      [now]
+    );
+    const gameId = gameInsertResult[0].insertId;
+
+    const turnInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      "insert into turns (game_id, turn_count, next_disc, end_at) values (?, ?, ?, ?)",
+      [gameId, 0, DARK, now]
+    );
+    const turnId = turnInsertResult[0].insertId;
+
+    const squareCount = INITIAL_BOARD.map((line) => line.length).reduce(
+      (v1, v2) => v1 + v2,
+      0
+    );
+
+    const squaresInsertSql =
+      "insert into squares (turn_id, x, y, disc) values " +
+      Array.from(Array(squareCount))
+        .map(() => "(?, ?, ?, ?)")
+        .join(", ");
+
+    const squaresInsertValues: any[] = [];
+    INITIAL_BOARD.forEach((line, y) => {
+      line.forEach((disc, x) => {
+        squaresInsertValues.push(turnId);
+        squaresInsertValues.push(x);
+        squaresInsertValues.push(y);
+        squaresInsertValues.push(disc);
+      });
+    });
+
+    await conn.execute(squaresInsertSql, squaresInsertValues);
+
     await conn.commit();
   } finally {
     await conn.end();
@@ -37,16 +92,18 @@ app.post("/api/games", async (req, res) => {
 
 app.use(errorHandler);
 
+app.listen(PORT, () => {
+  console.log(`Reversi application started: http://localhost:${PORT}`);
+});
+
 function errorHandler(
-  err: Error,
-  req: express.Request,
+  err: any,
+  _req: express.Request,
   res: express.Response,
   _next: express.NextFunction
 ) {
-  console.error(err.stack);
-  res.status(500).json({ error: err.message });
+  console.error("Unexpected error occurred", err);
+  res.status(500).send({
+    message: "Unexpected error occurred",
+  });
 }
-
-app.listen(port, () => {
-  console.log(`Server is running on: http://localhost:${port}`);
-});

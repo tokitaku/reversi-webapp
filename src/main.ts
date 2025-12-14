9b3cfece32044cb587e9b3d2e26fd16b1e06d621
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-12-14 09:36:36 +0900

    feat: 最新ターンの移動処理を実装し、盤面の状態をデータベースに保存する機能を追加

--- src/main.ts
+++ src/main.ts
@@ -130,9 +130,80 @@ app.post("/api/games/latest/turns", async (req, res) => {
   const disc = parseInt(req.body.move.disc);
   const x = parseInt(req.body.move.x);
   const y = parseInt(req.body.move.y);
-  console.log(
-    `Received move: turnCount=${turnCount}, disc=${disc}, x=${x}, y=${y}`
-  );
+
+  const conn = await connectMySQL();
+  // 1つ前のターンを取得する
+  try {
+    const gameSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      "select id, started_at from games order by id desc limit 1"
+    );
+    const game = gameSelectResult[0][0];
+
+    const previousTurnCount = turnCount - 1;
+    const turnSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      "select id, game_id, turn_count, next_disc, end_at from turns where game_id = ? and turn_count = ?",
+      [game["id"], previousTurnCount]
+    );
+    const turn = turnSelectResult[0][0];
+
+    const squaresSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      `select id, turn_id, x, y, disc from squares where turn_id = ?`,
+      [turn["id"]]
+    );
+    const squares = squaresSelectResult[0];
+    const board = Array.from(Array(8)).map(() => Array.from(Array(8)));
+    squares.forEach((s) => {
+      board[s.y][s.x] = s.disc;
+    });
+    // 盤面におけるかチェックする
+
+    // 石を置く
+    board[y][x] = disc;
+
+    // ひっくり返す
+
+    // ターンを保存する
+    const nextDisc = disc === DARK ? LIGHT : DARK;
+    const now = new Date();
+    const turnInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      "insert into turns (game_id, turn_count, next_disc, end_at) values (?, ?, ?, ?)",
+      [game["id"], turnCount, nextDisc, now]
+    );
+    const turnId = turnInsertResult[0].insertId;
+
+    const squareCount = board
+      .map((line) => line.length)
+      .reduce((v1, v2) => v1 + v2, 0);
+
+    const squaresInsertSql =
+      "insert into squares (turn_id, x, y, disc) values " +
+      Array.from(Array(squareCount))
+        .map(() => "(?, ?, ?, ?)")
+        .join(", ");
+
+    const squaresInsertValues: any[] = [];
+    board.forEach((line, y) => {
+      line.forEach((disc, x) => {
+        squaresInsertValues.push(turnId);
+        squaresInsertValues.push(x);
+        squaresInsertValues.push(y);
+        squaresInsertValues.push(disc);
+      });
+    });
+
+    await conn.execute(squaresInsertSql, squaresInsertValues);
+
+    await conn.execute("insert into moves (turn_id, x, y) values (?, ?, ?)", [
+      turnId,
+      x,
+      y,
+    ]);
+
+    await conn.commit();
+  } finally {
+    await conn.end();
+  }
+
   res.status(201).end();
 });
 app.use(errorHandler);

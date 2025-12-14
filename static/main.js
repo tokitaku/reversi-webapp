019574365828cb29e052314a9ae92290dafd4980
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-12-14 09:58:28 +0900

    feat: showBoard関数にturnCount引数を追加し、初期ボード表示を修正

--- static/main.js
+++ static/main.js
@@ -4,8 +4,7 @@ const LIGHT = 2;
 
 const boardElement = document.getElementById("board");
 
-async function showBoard() {
-  const turnCount = 0;
+async function showBoard(turnCount) {
   const response = await fetch(`/api/games/latest/turns/${turnCount}`);
   const responseBody = await response.json();
   const board = responseBody.board;
@@ -33,6 +32,7 @@ async function showBoard() {
           const nextTurnCount = turnCount + 1;
 
           await registerTurn(nextTurnCount, nextDisc, x, y);
+          await showBoard(nextTurnCount);
         });
       }
 
@@ -67,7 +67,7 @@ async function registerTurn(turnCount, disc, x, y) {
 
 async function main() {
   await registerGame();
-  await showBoard();
+  await showBoard(0);
 }
 
 main();

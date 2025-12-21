2e29935a1f8d193e8470313d1081463702b605cc
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-12-22 08:36:59 +0900

    feat: 次のディスクのメッセージ表示を実装し、ターンの進行を明確化

--- static/main.js
+++ static/main.js
@@ -3,12 +3,15 @@ const DARK = 1;
 const LIGHT = 2;
 
 const boardElement = document.getElementById("board");
+const nextDiscMessageElement = document.getElementById("next-disc-message");
 
 async function showBoard(turnCount) {
   const response = await fetch(`/api/games/latest/turns/${turnCount}`);
   const responseBody = await response.json();
   const board = responseBody.board;
   const nextDisc = responseBody.nextDisc;
+
+  showNextDiscMessage(nextDisc);
 
   while (boardElement.firstChild) {
     boardElement.removeChild(boardElement.firstChild);
@@ -30,15 +33,31 @@ async function showBoard(turnCount) {
       } else {
         squareElement.addEventListener("click", async () => {
           const nextTurnCount = turnCount + 1;
+          const registerTurnResponse = await registerTurn(
+            nextTurnCount,
+            nextDisc,
+            x,
+            y
+          );
 
-          await registerTurn(nextTurnCount, nextDisc, x, y);
-          await showBoard(nextTurnCount);
+          if (registerTurnResponse.ok) {
+            await showBoard(nextTurnCount);
+          }
         });
       }
 
       boardElement.appendChild(squareElement);
     });
   });
+}
+
+function showNextDiscMessage(nextDisc) {
+  if (nextDisc) {
+    const color = nextDisc === DARK ? "黒" : "白";
+    nextDiscMessageElement.innerText = `次は${color}の番です`;
+  } else {
+    nextDiscMessageElement.innerText = ``;
+  }
 }
 
 async function registerGame() {
@@ -56,7 +75,7 @@ async function registerTurn(turnCount, disc, x, y) {
       y,
     },
   };
-  await fetch("/api/games/latest/turns", {
+  return await fetch("/api/games/latest/turns", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",

b8e01e23e870c4163aa3675d4b2729df27c56b51
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-12-27 09:38:50 +0900

    feat: ゲームのインターフェースを実装し、ゲーム結果を表示するテーブルを追加

--- static/main.js
+++ static/main.js
@@ -1,132 +1,27 @@
-const EMPTY = 0;
-const DARK = 1;
-const LIGHT = 2;
+const gamesTableBodyElement = document.getElementById("games-table-body");
 
-const WINNER_DRAW = 0;
-const WINNER_DARK = 1;
-const WINNER_LIGHT = 2;
+async function showGames() {
+  const response = await fetch("/api/games");
+  const responseBodyGames = await response.json();
+  const games = responseBodyGames.games;
 
-const boardElement = document.getElementById("board");
-const nextDiscMessageElement = document.getElementById("next-disc-message");
-const warningMessageElement = document.getElementById("warning-message");
+  for (const game of games) {
+    const trElement = document.createElement("tr");
 
-async function showBoard(turnCount, previousDisc) {
-  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
-  const responseBody = await response.json();
-  const board = responseBody.board;
-  const nextDisc = responseBody.nextDisc;
-  const winnerDisc = responseBody.winnerDisc;
+    const appendTdElement = (innerText) => {
+      const tdElement = document.createElement("td");
+      tdElement.textContent = innerText;
+      trElement.appendChild(tdElement);
+    };
 
-  showWarningMessage(previousDisc, nextDisc, winnerDisc);
+    appendTdElement(game.darkMoveCount);
+    appendTdElement(game.lightMoveCount);
+    appendTdElement(game.winnerDisc);
+    appendTdElement(game.startedAt);
+    appendTdElement(game.endAt);
 
-  showNextDiscMessage(nextDisc);
-
-  while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild);
-  }
-
-  board.forEach((line, y) => {
-    line.forEach((square, x) => {
-      // <div class="square">
-      const squareElement = document.createElement("div");
-      squareElement.className = "square";
-
-      if (square !== EMPTY) {
-        // <div class="stone dark">
-        const stoneElement = document.createElement("div");
-        const color = square === DARK ? "dark" : "light";
-        stoneElement.className = `stone ${color}`;
-
-        squareElement.appendChild(stoneElement);
-      } else {
-        squareElement.addEventListener("click", async () => {
-          const nextTurnCount = turnCount + 1;
-          const registerTurnResponse = await registerTurn(
-            nextTurnCount,
-            nextDisc,
-            x,
-            y
-          );
-          if (registerTurnResponse.ok) {
-            await showBoard(nextTurnCount, nextDisc);
-          }
-        });
-      }
-
-      boardElement.appendChild(squareElement);
-    });
-  });
-}
-
-function discToString(disc) {
-  return disc === DARK ? "黒" : "白";
-}
-
-function showWarningMessage(previousDisc, nextDisc, winnerDisc) {
-  const message = warningMessage(previousDisc, nextDisc, winnerDisc);
-
-  warningMessageElement.innerText = message;
-
-  if (message === null) {
-    warningMessageElement.style.display = "none";
-  } else {
-    warningMessageElement.style.display = "block";
+    gamesTableBodyElement.appendChild(trElement);
   }
 }
 
-function warningMessage(previousDisc, nextDisc, winnerDisc) {
-  if (nextDisc !== null) {
-    if (previousDisc === nextDisc) {
-      const skipped = nextDisc === DARK ? LIGHT : DARK;
-      return `${discToString(skipped)}の番はスキップです`;
-    } else {
-      return null;
-    }
-  } else {
-    if (winnerDisc === WINNER_DRAW) {
-      return "引き分けです";
-    } else {
-      return `${discToString(winnerDisc)}の勝ちです`;
-    }
-  }
-}
-
-function showNextDiscMessage(nextDisc) {
-  if (nextDisc) {
-    nextDiscMessageElement.innerText = `次は${discToString(nextDisc)}の番です`;
-  } else {
-    nextDiscMessageElement.innerText = "";
-  }
-}
-
-async function registerGame() {
-  await fetch("/api/games", {
-    method: "POST",
-  });
-}
-
-async function registerTurn(turnCount, disc, x, y) {
-  const requestBody = {
-    turnCount,
-    move: {
-      disc,
-      x,
-      y,
-    },
-  };
-
-  return await fetch("/api/games/latest/turns", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(requestBody),
-  });
-}
-
-async function main() {
-  await registerGame();
-  await showBoard(0);
-}
-
-main();
+showGames();
